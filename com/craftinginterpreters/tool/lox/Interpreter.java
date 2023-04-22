d22d8d7420358b4c676e710ece84edfe906a96d4
Ezra <E.alaganou@gmail.com>
2023-04-22 17:10:17 -0400

    interpreter

---
+++ com/craftinginterpreters/tool/lox/Interpreter.java
@@ -0,0 +1,61 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+            return -(double)right;
+        }
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+
+    }
+        // Unreachable.
+        return null;
+    }
+    @Override
+    public Object visitGroupingExpr(Expr.grouping expr) {
+        return evaluate(expr.expression);
+    
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+        
+    }
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String) right;
+                }
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+        // Unreachable.
+        return null;
+    }
+    }
+    }
+}
